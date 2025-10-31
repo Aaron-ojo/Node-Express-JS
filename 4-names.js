67aafe3fa80bc9ccb9aa340f77168226b05e897e
Aaron-ojo <168048069+Aaron-ojo@users.noreply.github.com>
2025-10-30 18:29:43 -0700

    fs

--- 4-names.js
+++ 4-names.js
@@ -4,4 +4,4 @@ const secret = "Super Secret";
 const john = "john";
 const peter = "peter";
 
-module.exports = { john, peter };
+module.exports = { john,peter };
--- 9-path-modules.js
+++ 9-path-modules.js
@@ -1,6 +1,6 @@
-const path = require("path");
+// const path = require("path");
 
-console.log(path.sep);
+// console.log(path.sep);
 
 const filePath = path.join("/content", "subfolder", "text.txt");
 console.log(filePath);
