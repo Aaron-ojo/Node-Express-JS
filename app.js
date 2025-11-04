7d7485ecaee6eca109998e398bd8e9144ceb53d4
Aaron-ojo <168048069+Aaron-ojo@users.noreply.github.com>
2025-11-04 10:32:50 -0800

    p

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-// const { readFileSync, writeFileSync } = require("fs");
-// console.log("start");
-// const first = readFileSync("./content/first.txt", "utf8");
-// const second = readFileSync("./content/second.txt", "utf8");
-
-// writeFileSync(
-//   "./content/result-sync.txt",
-//   `Here is the result : ${first}, ${second}`,
-//   { flag: "a" }
-// );
-// console.log("done with this task");
-// console.log("starting the next one");
+const _ = require("lodash");
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
